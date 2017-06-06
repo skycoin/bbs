683d224538684a6d858d448042522afb3c96053d
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2017-06-07 00:21:14 +1200

    Comments.

--- run_test_mode.sh
+++ run_test_mode.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 echo "[ STARTING BBS NODE ]"
+# Set test-mode-timeout=-1 to disable.
+# Set test-mode-post-cap=-1 to disable.
 go run $GOPATH/src/github.com/skycoin/bbs/cmd/bbsnode/bbsnode.go \
     --test-mode=true \
     --test-mode-threads=4 \
     --test-mode-users=50 \
     --test-mode-min=0 \
-    --test-mode-max=1 \
-    --test-mode-timeout=5 \
+    --test-mode-max=0 \
+    --test-mode-timeout=-1 \
     --test-mode-post-cap=200 \
     --cxo-use-internal=true
 echo "Goodbye!"
