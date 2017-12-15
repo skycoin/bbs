9fdc7e18a6f03ecc1218394d4a678cf54d523384
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2017-12-15 21:18:18 +1300

    changed addresses

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ pushd "$DIR" >/dev/null
 
 go run cmd/bbsnode/bbsnode.go \
     -web-gui-dir="${DIR}/static/dist" \
-    -enforced-messenger-addresses="messenger.skycoin.net:8080" \
-    -enforced-subscriptions="03588a2c8085e37ece47aec50e1e856e70f893f7f802cb4f92d52c81c4c3212742" \
+    -enforced-messenger-addresses="35.185.110.6:8080" \
+    -enforced-subscriptions="03cfd850ed4df2e43c8e6359c00de574df4a233b710ee44252166a5097f1c6056d" \
     -open-browser=true
     $@
 
